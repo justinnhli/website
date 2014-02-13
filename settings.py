12d6870d5d14833b8f8a73a1dcde784b01ffa2aa
justinnhli <justinnhli@gmail.com>
2014-02-13 17:41:51 -0500

    more links, disable tags

--- settings.py
+++ settings.py
@@ -2,10 +2,16 @@ AUTHOR = "Justin Li"
 SITENAME = "Justin Li"
 SITEURL = "http://justinnhli.com"
 
-DATE_FORMATS = {'en':"%Y-%m-%d, %a",}
-TIMEZONE = "America/Detroit"
+MENUITEMS = (
+    ("Essays", "/archives.html"),
+    ("Code", "https://github.com/justinnhli?tab=repositories"),
+    ("Twitter", "https://twitter.com/justinnhli/"),
+)
 
 RELATIVE_URLS = True
+
+DATE_FORMATS = {'en':"%Y-%m-%d, %a",}
+TIMEZONE = "America/Detroit"
 
 THEME = "themes/justinnhli"
 
@@ -13,3 +19,7 @@ USE_FOLDER_AS_CATEGORY = False
 DISPLAY_CATEGORIES_ON_MENU = False
 
 FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+TAGS_SAVE_AS = ''
+TAG_SAVE_AS = ''
